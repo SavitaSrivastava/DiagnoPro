ba86118a017b1cb2a057f2626f4cebc1291e29f3
SavitaSrivastava <savita.srivasatava@globalindianschool.org>
2023-10-19 19:43:34 +0530

    App code

---
+++ DiagnoPro/app/src/main/java/com/ndkapp/www/diagnopro/activity_contact_us.java
@@ -0,0 +1,13 @@
+package com.ndkapp.www.diagnopro;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_contact_us extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact_us);
+    }
+}
